620d3b1e8ecf2e91991540d8f9013f478ee66c4e
Ronald <ronaldmacario13@gmail.com>
2024-07-07 18:58:03 -0300

    feat: Refactor EnemyController for improved movement and detection

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -4,22 +4,47 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] private EnemyObject enemySettings;
 
-    private string enemyName;
-    private float health;
-    private float speed;
-    private float attack;
+    private string _name;
+
+    private float _speed = 3.5f;
+    private Vector2 _direction;
+    private Rigidbody2D _rigidbody;
+
+    [SerializeField] private DetectionController _detectionArea;
+    
+    private float _health;
+    private float _attack;
 
     private Animator animator;
 
-    void Start() {
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
         animator = GetComponent<Animator>();
 
         animator.runtimeAnimatorController = enemySettings.animatorController;
 
-        enemyName = enemySettings.enemyName;
-        health = enemySettings.health;
-        speed = enemySettings.speed;
-        attack = enemySettings.attack;
+        _name = enemySettings.enemyName;
+        _health = enemySettings.health;
+        _speed = enemySettings.speed;
+        _attack = enemySettings.attack;
+
+    }
+
+    void Update()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        if(_detectionArea.detectedObjs.Count > 0)
+        {
+            _direction = (_detectionArea.detectedObjs[0].transform.position - transform.position).normalized;
+
+            _rigidbody.MovePosition(_rigidbody.position + _direction * _speed * Time.fixedDeltaTime);
+        }
     }
 
 }
