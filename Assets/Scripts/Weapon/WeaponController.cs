b47dc8f85adc7621756806cc196492a7084eb167
Ronald <ronaldmacario13@gmail.com>
2024-07-20 20:13:27 -0300

    feat: Update WeaponController to use WeaponObject for configurable weapon settings

--- Assets/Scripts/Weapon/WeaponController.cs
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -2,54 +2,62 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    // Wearpon Characteristics
-    [SerializeField] private int _damage = 1;
-    
-    // Components
-    [SerializeField] private PlayerController _player;
-    private Rigidbody2D _weaponRigidbody2D;
-    
-    // Flip config
-    private Vector2 _playerDirection;
-    private bool _rightPosition = true;
+  // Wearpon Characteristics
+  [SerializeField] private WeaponObject _weaponSettings;
+  [SerializeField] private int _damage;
+  
+  // Components
+  [SerializeField] private PlayerController _player;
+  private Rigidbody2D _weaponRigidbody2D;
+  
+  // Flip config
+  private Vector2 _playerDirection;
+  private bool _rightPosition = true;
 
-    void Start()
+  private Animator animator;
+
+  void Start()
+  {
+    _weaponRigidbody2D = GetComponent<Rigidbody2D>();
+
+    _damage = _weaponSettings._damage;
+
+    animator = GetComponent<Animator>();
+    animator.runtimeAnimatorController = _weaponSettings._animatorController;
+  }
+
+  void Update()
+  {
+    _playerDirection = _player._playerDirection;
+
+    if (_playerDirection.x == 1.0f)
     {
-        _weaponRigidbody2D = GetComponent<Rigidbody2D>();
+      _rightPosition = true;
     }
-
-    void Update()
+    else if (_playerDirection.x == -1.0f)
     {
-        _playerDirection = _player._playerDirection;
-
-        if (_playerDirection.x == 1.0f)
-        {
-            _rightPosition = true;
-        }
-        else if (_playerDirection.x == -1.0f)
-        {
-            _rightPosition = false;
-        }
-
-        FlipWeaponPosition();
+      _rightPosition = false;
     }
 
-    void FlipWeaponPosition()
+    FlipWeaponPosition();
+  }
+
+  void FlipWeaponPosition()
+  {
+    if(_rightPosition)
     {
-        if(_rightPosition)
-        {
-            _weaponRigidbody2D.MovePosition(_player.transform.position + new Vector3(1.0f, 0.0f));
-        }
-        else 
-        {
-            _weaponRigidbody2D.MovePosition(_player.transform.position + new Vector3(-1.0f, 0.0f));
-        }
+      _weaponRigidbody2D.MovePosition(_player.transform.position + new Vector3(1.0f, 0.0f));
     }
+    else 
+    {
+      _weaponRigidbody2D.MovePosition(_player.transform.position + new Vector3(-1.0f, 0.0f));
+    }
+  }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.Equals(GameObject.FindGameObjectsWithTag("Enemy")))
-        {
-            other.GetComponent<EnemyController>()._health =- _damage;
-        }
+  private void OnTriggerEnter2D(Collider2D other) {
+    if(other.Equals(GameObject.FindGameObjectsWithTag("Enemy")))
+    {
+      other.GetComponent<EnemyController>()._health =- _damage;
     }
+  }
 }
--- Assets/Scripts/Weapon/WeaponObject.cs
+++ Assets/Scripts/Weapon/WeaponObject.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "WeaponController", menuName = "Weapon/New Weapon Config")]
 public class WeaponObject : ScriptableObject {
-    
+  public int _damage;
+
+  public RuntimeAnimatorController _animatorController;
 }
