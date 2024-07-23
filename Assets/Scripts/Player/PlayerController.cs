e1a959767dc5edff81275dc015a6bff4f3112ecc
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2024-07-23 14:36:23 -0300

    feat: adicionando animações do player, do pneu e do mosquito morrendo

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,8 +5,10 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D playerRigidbody;
     [SerializeField] private float playerSpeed = 7;
+    private Animator _playerAnimator;
     private float playerInitialSpeed;
     private Vector2 playerDirection;
+    private SpriteRenderer _spriteRenderer;
 
     private bool isAttack = false;
 
@@ -14,6 +16,10 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+
+        _playerAnimator = GetComponent<Animator>();
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
 
         playerInitialSpeed = playerSpeed;
     }
@@ -24,6 +30,15 @@ public class PlayerController : MonoBehaviour
         playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         OnAttack();
+
+        if (playerDirection.sqrMagnitude > 0)
+        {
+            _playerAnimator.SetBool("isWalking", true); 
+        } else {
+            _playerAnimator.SetBool("isWalking", false);
+        }
+
+        Flip();
     }
 
     // Esse método é chamado a cada 0.2 segundos (Para um melhor controle da física do jogo deve ser feita aqui 
@@ -37,6 +52,8 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetMouseButtonDown(0))
         {
+            _playerAnimator.SetBool("racket", true);
+            _playerAnimator.SetTrigger("isAttacking");
             Debug.Log("Atacando!");
             isAttack = true;
             playerSpeed = 0;
@@ -50,4 +67,13 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void Flip() {
+        if (playerDirection.x > 0)
+        {
+            _spriteRenderer.flipX = false;
+        } else if(playerDirection.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+    }
 }
